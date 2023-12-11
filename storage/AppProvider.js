ee77963a82555a2e64ac1418817cb9d040bbb171
Asma <asmaa.shabaan@vodafone.com>
2023-12-11 04:04:42 +0200

    f

--- storage/AppProvider.js
+++ storage/AppProvider.js
@@ -20,7 +20,7 @@ export const AppProvider = ({ children }) => {
   return (
     <AppContext.Provider
       value={{
-       theme: !darkMode ? lightModeColors : darkModeColors,
+       theme: !darkMode ? lightModeColors : darkModeColors, // here is problem with this property 
         language,
         darkMode,
         toggleLanguage,
